82cc8202ae7afb0f6d513d68e15e3907c55dffbb
Xiaoyi Chen <xchen84@illinois.edu>
2018-05-08 14:36:50 +0800

    make header bg img changable

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header img="1"/>
         <Container>
           <Row>
             <Column width='12'>
